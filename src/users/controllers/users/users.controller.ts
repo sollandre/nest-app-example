dba928e83f6113ba0d8f35bd62c90ec54aa84fa0
sollandre <andrebmsolla@gmail.com>
2023-03-18 00:17:57 -0300

    First nest app finished

---
+++ src/users/controllers/users/users.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Res,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { CreateUserDto } from 'src/users/dto/users.dtos';
+import { UsersService } from 'src/users/services/users/users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get()
+  getUsers() {
+    return this.userService.getUsers();
+  }
+
+  @Get(':id')
+  findUsersById(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findUsersById(id);
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createUsers(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id', ParseIntPipe) id: number, @Res() res: Response) {
+    this.userService
+      .deleteUser(id)
+      .then(() => {
+        return res.status(203).send();
+      })
+      .catch((error) => {
+        return res.status(418).json({ error }).send();
+      });
+  }
+}
---
+++ src/users/services/users/users.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/typeorm';
+import { CreateUserDto } from 'src/users/dto/users.dtos';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  createUser(createUserDto: CreateUserDto) {
+    const newUser = this.userRepository.create(createUserDto);
+    return this.userRepository.save(newUser);
+  }
+
+  findUsersById(id: number) {
+    return this.userRepository.findOneBy({
+      id,
+    });
+  }
+
+  async getUsers() {
+    const data = await this.userRepository.find();
+    if (data.length <= 0) {
+      return { message: 'No data found' };
+    } else {
+      return data;
+    }
+  }
+
+  async deleteUser(id: number) {
+    const userExists = await this.userRepository.exist({
+      where: {
+        id,
+      },
+    });
+    if (!userExists) {
+      console.log('entrei');
+      return Promise.reject({ message: 'Id not found' });
+    }
+
+    this.userRepository.delete({
+      id,
+    });
+  }
+}
